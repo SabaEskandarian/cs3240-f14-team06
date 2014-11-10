4f09531babc432779adc9918ec0069c013591201
Carina Cai <yc5bd@virginia.edu>
2014-11-10 11:05:52 -0800

    added a css file

--- project/SecureWitness/admin.py
+++ project/SecureWitness/admin.py
@@ -4,8 +4,19 @@ from SecureWitness.models import Document
 from SecureWitness.models import Bulletin
 from SecureWitness.models import Sharing
 
+class BulletinInline(admin.StackedInline):
+	model = Bulletin
+	extra = 3
+
+class FolderAdmin(admin.ModelAdmin):
+	fields=['user','name']
+	inlines = [BulletinInline]
+
+class BulletinAdmin(admin.ModelAdmin):
+	fields=['name','date']
+
 # Register your models here.
-admin.site.register(Folder)
-admin.site.register(Bulletin)
+admin.site.register(Folder, FolderAdmin)
+admin.site.register(Bulletin,BulletinAdmin)
 admin.site.register(Document)
 admin.site.register(Sharing)
